c1063ece03f7a6c6845590823b5379d51c7d1b4e
Diego Freitas de Sousa <diego.freitas.professional@outlook.com>
2024-05-17 17:57:03 -0300

    [wip] atualiznado repositorio

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 const measurement = require('./lib/measurement')
 const event = require('./lib/event')
-const stations = require('./data/stations')
+let stations = require('./data/stations')
+const station_prefix = require('./lib/statio_prefix')
 
-
-
-measurement.getMeasurements({station_prefix_ids: [33212], start_date: '2023-12-22 03:00', end_date: '2023-12-23 02:59', group_type: 'minute'}).then(response=>{
-    response.data.json.map(x=>{
-        stations[x.station_prefix_id] = stations[x.station_prefix_id] || {}
-        stations[x.station_prefix_id].measurements = stations[x.station_prefix_id].measurements || []
-        stations[x.station_prefix_id].measurements.push(x)
-        stations[x.station_prefix_id].station_type_id = x.station_type_id //salvando o tipo do posto
-    })
+station_prefix.getStations().then(response=>{
+    let s = response.data
+    stations = s.map(x=>x.id)
+    // console.log(stations.map(x=>x.id).length);
+    start([114])
     
-    event.getRainEvents(1)
-    
-    event.getLevelEvents('attention')
-    
-    // console.log(stations[33212].extravasation_events)
 })
-.catch(data=>{
-    console.error(data.response)
-});
+
+const start = async stations =>{
+    let start = 0   
+    while (start < stations.length){
+        const chunk = stations.slice(start, start + 20);
+        await measurement.getMeasurements({station_prefix_ids: chunk, start_date: new Date(new Date().getTime() - (24 * 60 * 60 * 1000)).toLocaleString(), end_date: new Date(), group_type: 'minute'}).then(response=>{
+            let _stations = {}
+            response.data.json.map(x=>{
+                _stations[x.station_prefix_id] = _stations[x.station_prefix_id] || {id:x.station_prefix_id}
+                _stations[x.station_prefix_id].measurements = _stations[x.station_prefix_id].measurements || []
+                _stations[x.station_prefix_id].measurements.push(x)
+                _stations[x.station_prefix_id].station_type_id = x.station_type_id //salvando o tipo do posto
+                _stations[x.station_prefix_id].measurement_gap = x.measurement_gap //salvando measurement_gap
+            })
+            
+            event.getRainEvents(_stations,3) //produzindo eventos de chuva para aos postos plu
+            event.saveEvents(_stations)
+            // event.getLevelEvents(['alert','attention', 'extravasation']).then(_=>{ //produzindo eventos de nivel para os postos flu
+            //     event.saveEvents()
+            // })
+            
+            // console.log(stations[33212].extravasation_events)
+        })
+        .catch(data=>{
+            start += 20
+        }).finally(_=>{
+            start += 20
+        })
+    }
+}
+
+
+
 
 return true
